b155f229dbcb4bf15ca8a40cdb02a1b99bd15198
Andrej Kvasnica <kvasnica@posam.sk>
2017-10-20 14:51:27 +0200

    cleanup dat - test data do @Before setupu

--- src/test/java/sk/posam/objednavky/domain/objednavka/TerminObjednavkyTest.java
+++ src/test/java/sk/posam/objednavky/domain/objednavka/TerminObjednavkyTest.java
@@ -2,24 +2,24 @@ package sk.posam.objednavky.domain.objednavka;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TerminObjednavkyTest {
 
+	private TerminObjednavky terminObjednavky10_30;
+	private TerminObjednavky terminObjednavky10_20;
+	private TerminObjednavky terminObjednavky15_20;
+
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		terminObjednavky10_30 = TerminObjednavkyFactory.create(10, 30);
+		terminObjednavky10_20 = TerminObjednavkyFactory.create(10, 20);
+		terminObjednavky15_20 = TerminObjednavkyFactory.create(15, 20);
 	}
 
 	@Test
 	public void testTerminObjednavky10_30SaPrekryvaSObjednavkou10_20() {
-		TerminObjednavky terminObjednavky10_30 = TerminObjednavkyFactory.create(10, 30);
-		TerminObjednavky terminObjednavky10_20 = TerminObjednavkyFactory.create(10, 20);
 
 		assertTrue(terminObjednavky10_30.prekryvaSa(terminObjednavky10_20));
 		assertTrue(terminObjednavky10_20.prekryvaSa(terminObjednavky10_30));
@@ -27,8 +27,6 @@ public class TerminObjednavkyTest {
 
 	@Test
 	public void testTerminObjednavky10_30SaNeprekryvaSObjednavkou15_20() {
-		TerminObjednavky terminObjednavky10_30 = TerminObjednavkyFactory.create(10, 30);
-		TerminObjednavky terminObjednavky15_20 = TerminObjednavkyFactory.create(15, 20);
 
 		assertFalse(terminObjednavky10_30.prekryvaSa(terminObjednavky15_20));
 		assertFalse(terminObjednavky15_20.prekryvaSa(terminObjednavky10_30));
